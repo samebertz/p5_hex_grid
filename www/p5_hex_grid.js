d4fc3dc823968e6e7ca17da3621c5ac0906790b8
Sam Ebertz <samuel.ebertz@gmail.com>
2018-03-01 20:46:55 -0600

    working hex draw from axial coordinates

--- www/p5_hex_grid.js
+++ www/p5_hex_grid.js
@@ -1,53 +1,69 @@
-const SEAMSEALER = 0.0,
-      HEX_SIZE = 100
+// canvas pixel coordinates are pairs (2 element arrays)
+// with x = pair[0] and y = pair[1]
+// axial coordinates are objects with properties q and r
+
+const HEX_SIZE     = 100,
+      HEX_HEIGHT   = HEX_SIZE * 2,
+      HEX_WIDTH    = HEX_HEIGHT * Math.sqrt(3)/2,
+      // Axial coordinate system origin in Cartesian coordinates
+      AXIAL_ORIGIN = [0, 0],
+      // Axial coordinate basis vectors in Cartesian coordinates,
+      // where q = +x and r = +z in cube coordinates
+      AXIAL_BASIS  = {
+        q: [HEX_WIDTH,   0],
+        r: [HEX_WIDTH/2, HEX_HEIGHT * 3/4]
+      }
 
 function setup() {
-  // put setup code here
-  // let container = document.getElementById('p5canvas_wrapper')
-  // var cnv = createCanvas()
-  // cnv.parent(container)
-  // resizeCanvas(container.width - 16, windowHeight - 10)
-  var cnv = createCanvas(HEX_SIZE*3/2*6.5 + 10, HEX_SIZE*11 - 35)
-  // cnv.style('border: 1px solid #000;')
+  createCanvas(windowWidth - 20, windowHeight - 20)
+
+  AXIAL_ORIGIN[0] = width/2
+  AXIAL_ORIGIN[1] = height/2
+
   background(255)
+  strokeWeight(3)
   // noStroke()
   noFill()
   noLoop()
 }
 
+// returns pixel coordinates of center of hex at axial coordinate <q,r>
+function hex_axial_to_pixel(hex_axial_coordinates) {
+  // var x = HEX_SIZE * sqrt(3)/2 * (hex_axial_coordinates.q + hex_axial_coordinates.r / 2)
+  var x = AXIAL_ORIGIN[0] + AXIAL_BASIS.q[0] * hex_axial_coordinates.q + AXIAL_BASIS.r[0] * hex_axial_coordinates.r
+  // var y = HEX_SIZE * 3/2 * hex_axial_coordinates.r
+  var y = AXIAL_ORIGIN[1] + AXIAL_BASIS.q[1] * hex_axial_coordinates.q + AXIAL_BASIS.r[1] * hex_axial_coordinates.r
+  return [x,y]
+}
+
+// returns the i_th corner of the hex with center and size
+function hex_corner(center, size, i) {
+  var angle = (i * 60 + 30) * Math.PI / 180
+  return [center[0] + size*Math.cos(angle),
+          center[1] + size*Math.sin(angle)]
+}
+
 function draw() {
-  // put drawing code here
-  function hex_center(coord, size) {
-    let height = size*2, width = height*Math.sqrt(3)/2
-    return {
-      x: HEX_SIZE + width*coord[0],
-      y: HEX_SIZE + 3/2*height*coord[1] + 10
-    }
-  }
-  function hex_corner(center, size, i) {
-    let angle = (i * 60 + 30) * Math.PI / 180
-    return [center.x + size*Math.cos(angle),
-            center.y + size*Math.sin(angle)]
-  }
-  function hex(coord, size, hl=false) {
-    let center = hex_center(coord, size)
+  ellipse(...AXIAL_ORIGIN, 25)
+
+  function draw_hex(hex_axial_coordinates) {
+    var center = hex_axial_to_pixel(hex_axial_coordinates)
     beginShape()
     for(let i=0; i<6; i++) {
-      vertex(...hex_corner(center, size, i))
+      vertex(...hex_corner(center, HEX_SIZE, i))
     }
     endShape(CLOSE)
-    // translate(center.x, center.y)
-    // if(hl) {fill(hl)} else {fill(200)}
-    // translate(-center.x, -center.y)
   }
-  function hex_grid(cols, rows) {
-    let height = HEX_SIZE * 2,
-        width = height * Math.sqrt(3)/2
-    for(let col = 0; col < cols; col++) {
-      for(let row = 0; row < rows; row++) {
-        hex([col, row], HEX_SIZE)
+  // draw_hex({q:0,r:0})
+  // draw_hex({q:0,r:1})
+
+  // function draw_hex_grid(cols, rows) {
+    var N = 3
+    for(let row = 0; row < N; row++) {
+      for(let col = 0; col < N; col++) {
+        if(row + col < N) draw_hex({q: col, r: row})
       }
     }
-  }
-  hex_grid(5, 3)
+  // }
+  // draw_hex_grid()
 }
